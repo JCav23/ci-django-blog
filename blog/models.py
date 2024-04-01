1d75705f455117248e80ab74a35b6a271d400b9c
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2024-04-01 10:42:30 +0000

    updated fields based on migration lesson material

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,5 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(max_length=50, blank=True, unique=True)
+    updated_on = models.DateTimeField(auto_now=True)
